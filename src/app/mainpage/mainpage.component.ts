e8201745ea4408df37f9afc0ad5bd7a656403f8f
caophil <philippcao60@gmail.com>
2023-06-04 22:11:33 +0200

    suche geht

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Eingabe } from '../dataclass/Eingabe';
+import { firstValueFrom,Observable, throwError} from 'rxjs';
+import { Result } from '../dataclass/Result';
 
 @Component({
   selector: 'app-mainpage',
@@ -13,6 +15,8 @@ export class MainpageComponent {
   constructor(private readonly router: Router,private http: HttpClient){}
 
   eingabe: string ="";
+  results!: Result[];
+  visible = true;
 
   async navigateLinkAdd(){
     this.router.navigate(['/linkAdd']);
@@ -20,6 +24,12 @@ export class MainpageComponent {
 
   async getResults(){
     var newEingabe: Eingabe = new Eingabe(this.eingabe);
+    try {
+      this.results = await firstValueFrom(this.http.post<Result[]>("http://localhost:3000/request", newEingabe));
+      this.visible = true;
+    } catch (error) {
+      alert((error as Error).message)
+    }
 
   }
 
